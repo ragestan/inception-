bd14d5c93ba8c24219b775e1a2c91375e4d49aa4
zbentalh <zbentalh@inception.myguest.virtualbox.org>
2025-01-16 22:29:29 +0100

    docker-compose done mariadb need debug

--- WordPress/conf/auto_config.sh
+++ WordPress/conf/auto_config.sh
@@ -2,4 +2,5 @@
 
 sleep 10
 wp config create	--allow-root --dbhost=$DBHOST --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBPASS --dbhost=mariadb:3306 --path='/var/www/wordpress'
+sed -i -e 's/.*listen = .*/listen = 9000/' /etc/php/7.4/fpm/pool.d/www.conf
 /usr/sbin/php-fpm7.3 -F
