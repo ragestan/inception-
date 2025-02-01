1780d3524cbdcf9a00c0368d58ba6e356e5a1d91
ragestan <you@example.com>
2025-02-01 14:55:35 +0100

    update

--- WordPress/conf/auto_config.sh
+++ WordPress/conf/auto_config.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 sleep 10
-wp config create	--allow-root --dbhost=$DBHOST --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBPASS --dbhost=mariadb:3306 --path='/var/www/wordpress'
-sed -i -e 's/.*listen = .*/listen = 9000/' /etc/php/7.4/fpm/pool.d/www.conf
+wp config create	--allow-root --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBPASS --dbhost=mariadb:3306 --path='/var/www/wordpress'
+sed -i -e 's/.*listen = .*/listen = 9000/' /etc/php/7.3/fpm/pool.d/www.conf
+
+
 /usr/sbin/php-fpm7.3 -F
