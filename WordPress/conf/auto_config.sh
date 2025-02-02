1a582fb975ba9e3654626d65a6e01d866981041f
ragestan <you@example.com>
2025-02-02 17:24:17 +0100

    update

--- WordPress/conf/auto_config.sh
+++ WordPress/conf/auto_config.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+echo "listen = wordpress:9000" >> /etc/php/7.3/fpm/pool.d/www.conf
+
 sleep 10
+
 wp config create	--allow-root --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBPASS --dbhost=mariadb:3306 --path='/var/www/wordpress'
-sed -i -e 's/.*listen = .*/listen = 9000/' /etc/php/7.3/fpm/pool.d/www.conf
+
+#sed -i -e 's/.*listen = .*/listen = 9000/' /etc/php/7.3/fpm/pool.d/www.conf
 
 
-/usr/sbin/php-fpm7.3 -F
+exec php-fpm7.3 -F
+
+
+
+
+#sleep 10
+#wp core download --allow-root
+#wp config create --dbhost=$DBHOST --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBPASS --allow-root
+#wp core install --url=$URL --title=$TITLE --admin_user=$ADMIN --admin_password=$ADMINPASS --admin_email=$ADMINEMAIL --allow-root
+#wp user create $USER $USEREMAIL --role=subscriber --user_pass=$USERPASS --allow-root
+#wp theme install twentytwentytwo --activate --allow-root
+#sed -i -e 's/.*listen = .*/listen = 9000/' /etc/php/7.3/fpm/pool.d/www.conf
+#php-fpm7.3 -F
