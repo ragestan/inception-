c51e87b8a9c1b86ab3048c6a45cd185212a9851e
ragestan <you@example.com>
2025-02-04 14:46:39 +0100

    done ?

--- WordPress/conf/auto_config.sh
+++ WordPress/conf/auto_config.sh
@@ -4,8 +4,12 @@ echo "listen = wordpress:9000" >> /etc/php/7.3/fpm/pool.d/www.conf
 
 sleep 10
 
-wp config create	--allow-root --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBPASS --dbhost=mariadb:3306 --path='/var/www/wordpress'
+wp config create --allow-root --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBPASS --dbhost=mariadb:3306 --path='/var/www/html'
 
+wp core install --url=$URL --title=$TITLE --admin_user=$ADMIN --admin_password=$ADMINPASS --admin_email=$ADMINEMAIL --allow-root
+
+wp user create $USER $USEREMAIL --role=subscriber --user_pass=$USERPASS --allow-root
+wp theme install twentytwentytwo --activate --allow-root
 #sed -i -e 's/.*listen = .*/listen = 9000/' /etc/php/7.3/fpm/pool.d/www.conf
 
 
